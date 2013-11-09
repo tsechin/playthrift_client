b7727bbc39079f50f246201866d6055377a47e1b
TEO Tse Chin <tsechin@tsechin.com>
2013-11-10 01:19:35 +0800

    script to build java code from thrift IDL

--- thriftgen.sh
+++ thriftgen.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 src_dir=src/main/java
 thrift_file=src/main/thrift/mathservice.thrift
-java_package=$(grep '^namespace java' ${thriftfile} | cut -d' ' -f 3)
+java_package=$(cat ${thrift_file} | grep '^namespace java' | cut -d' ' -f 3)
+echo "thrift IDL ${thrift_file}, java package ${java_package}"
 java_package_path=$(echo ${java_package} | sed -E 's/\./\//g')
+echo "java package path ${java_package_path}"
 
 [ -d ${src_dir}/${java_package_path} ] && rm -r ${src_dir}/${java_package_path}
-thrift --gen java -out ${src_dir} ${thriftfile}
+thrift --gen java -out ${src_dir} ${thrift_file}
