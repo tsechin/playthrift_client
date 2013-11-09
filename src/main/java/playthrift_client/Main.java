cb4d0dff5662147c80e9ad1a575b8d9d6f2b52b3
TEO Tse Chin <tsechin@tsechin.com>
2013-11-10 01:23:43 +0800

    use explicit HttpClient. put url in a single string.

--- src/main/java/playthrift_client/Main.java
+++ src/main/java/playthrift_client/Main.java
@@ -1,20 +1,22 @@
 package playthrift_client;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.THttpClient;
 import thrift_mathservice.MathService;
 
 public class Main {
-    private static final String schemeserverport = "http://localhost:9000";
-    private static final String url = "/math";
-
+    private static final String url = "http://localhost:9000/math";
+                                                            s
 	public static void main(String args[]) {
         try {
 
             System.out.println("playthrift client starting");
 
-            THttpClient tHttpClient = new THttpClient(schemeserverport+url);
+            HttpClient hc = new DefaultHttpClient();
+            THttpClient tHttpClient = new THttpClient(url, hc);
             TProtocol tProtocol = new TBinaryProtocol(tHttpClient);
 
             MathService.Client client = new MathService.Client(tProtocol);
